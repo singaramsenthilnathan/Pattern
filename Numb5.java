e5da43b29bda53f4073ffd16c92c20d75e4428b0
singaramsenthilnathan <ssingarammech@gmail.com>
2021-04-10 09:26:53 +0530

    Patterns

---
+++ Numb5.java
@@ -0,0 +1,33 @@
+/*
+7654321
+765432
+76543
+7654
+765
+76
+7*/
+public class Numb5
+{
+public static void main(String[]args)
+{
+for(int i=1;i<=7;i++)
+{
+for(int j=7;j>=i;j--)
+{
+System.out.print(j);
+}
+System.out.println();
+}
+
+
+/* OUTPUT:
+7654321
+765432
+76543
+7654
+765
+76
+7
+*/
+}
+}
---
+++ Numb6.java
@@ -0,0 +1,32 @@
+/*
+7
+76
+765
+7654
+76543
+765432
+7654321*/
+public class Numb6
+{
+public static void main(String[]args)
+{
+for(int i=7;i>=1;i--)
+{
+for(int j=7;j>=i;j--)
+{
+System.out.print(j);
+}
+System.out.println();
+}
+}
+}
+
+/* OUTPUT:
+7
+76
+765
+7654
+76543
+765432
+7654321
+*/
