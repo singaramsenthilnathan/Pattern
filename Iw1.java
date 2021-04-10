d9af77aad9be68e77776d3079219e7e96bff443d
singaramsenthilnathan <ssingarammech@gmail.com>
2021-04-10 19:19:24 +0530

    Patterns

---
+++ Iw1.java
@@ -0,0 +1,42 @@
+/*
+1
+1 0
+1 0 1
+1 0 1 0
+1 0 1 0 1
+1 0 1 0 1 0
+1 0 1 0 1 0 1*/
+
+public class Iw1
+{
+public static void main(String []args)
+{
+for(int i=1;i<=7;i++)
+{
+for(int j=1;j<=i;j++)
+{
+if(j%2==0)
+{
+System.out.print("0"+" ");
+}
+else
+{
+System.out.print("1"+" ");
+}
+}
+System.out.println();
+}
+}
+}
+
+/*OUTPUT:
+
+1
+1 0
+1 0 1
+1 0 1 0
+1 0 1 0 1
+1 0 1 0 1 0
+1 0 1 0 1 0 1
+*/
+
