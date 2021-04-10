de33c4ce7efc8a5d6d6c6d2206ac96c05d9a4518
singaramsenthilnathan <ssingarammech@gmail.com>
2021-04-10 17:33:05 +0530

    Patterns

---
+++ To2.java
@@ -0,0 +1,33 @@
+/*
+55555
+4444
+333
+22
+1
+*/
+
+public class To2
+{
+public static void main(String[]args)
+{
+int n=5;
+for(int i=5;i>=1;i--)
+{
+for(int j=n;j>=1;j--)
+{
+System.out.print(n);
+}
+n--;
+System.out.println();
+}
+
+}
+}
+
+/* OUTPUT:
+55555
+4444
+333
+22
+1
+*/
