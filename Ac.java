5688387db18c0895fc4598efc2f9541cf8cdf3c8
singaramsenthilnathan <ssingarammech@gmail.com>
2021-04-10 16:46:42 +0530

    Patterns

---
+++ Ac.java
@@ -0,0 +1,39 @@
+public class Ac
+//A
+{
+public static void main(String[]args)
+{
+for(int i=5;i>=1;i--)
+{
+System.out.print("*");
+}
+System.out.println();
+for(int i=4;i>=1;i--)
+{
+System.out.println("*   *");
+}
+for(int i=5;i>=1;i--)
+{
+System.out.print("*");
+}
+System.out.println();
+for(int i=4;i>=1;i--)
+{
+System.out.println("*   *");
+}
+}
+}
+
+/*OUTPUT:
+*****
+*   *
+*   *
+*   *
+*   *
+*****
+*   *
+*   *
+*   *
+*   *
+
+*/
