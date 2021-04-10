b8273b9ffd50ea7ea39b4d10e2a67a87a350b555
singaramsenthilnathan <ssingarammech@gmail.com>
2021-04-10 17:25:42 +0530

    Patterns

---
+++ To1.java
@@ -0,0 +1,33 @@
+/*
+54321
+4321
+321
+21
+1
+*/
+
+public class To1
+{
+public static void main(String[]args)
+{
+int n=5;
+int m=1;
+for(int j=1;j<=5;j++)
+{
+for(int i=n;i>=m;i--)
+{
+System.out.print(i);
+}
+n--;
+System.out.println();
+}
+}
+}
+
+/* OUTPUT:
+54321
+4321
+321
+21
+1
+*/
