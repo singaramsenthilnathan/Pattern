4b773ebac7412ba0c253e49f4fca0dc310b56370
singaramsenthilnathan <ssingarammech@gmail.com>
2021-04-10 10:01:13 +0530

    Patterns

---
+++ Numb3.java
@@ -0,0 +1,58 @@
+/*
+1
+12
+123
+1234
+12345
+123456
+1234567
+123456
+12345
+1234
+123
+12
+1*/
+public class Numb3
+{
+	
+public static void main(String[]args)
+{
+for(int i=1;i<=7;i++)
+{
+for(int j=1;j<=i;j++)
+{
+	System.out.print(j);
+}
+System.out.println();
+}
+for(int k=6;k>=1;k--)
+{
+	for(int l=1;l<=k;l++)
+	{
+		System.out.print(l);
+	}
+	System.out.println();
+}
+}
+}
+/* OUTPUT:
+1
+12
+123
+1234
+12345
+123456
+1234567
+123456
+12345
+1234
+123
+12
+1
+*/
+
+
+
+
+
+
---
+++ Numb4.java
@@ -0,0 +1,52 @@
+/*
+1234567
+123456
+12345
+1234
+123
+12
+1
+12
+123
+1234
+12345
+123456
+1234567
+*/
+public class Numb4
+{
+public static void main(String[]args)
+{
+for(int i=7;i>=1;i--)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print(j);
+}
+System.out.println();
+}
+for(int k=2;k<=7;k++)
+{
+for(int l=1;l<=k;l++)
+{
+System.out.print(l);
+}
+System.out.println();
+}
+}
+}
+/* OUTPUT:
+1234567
+123456
+12345
+1234
+123
+12
+1
+12
+123
+1234
+12345
+123456
+1234567
+*/
