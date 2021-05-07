5a656598fe7423d66d5b0a52a2a82dffde9771fb
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-05-07 16:40:43 +0530

    Alphabet Pattern

---
+++ AlphPattern3.java
@@ -0,0 +1,24 @@
+package test.second;
+public class AlphPattern3 
+{
+	static int alphabet = 69;
+public static void main(String[] args) 
+{
+for(int i=5;i>=1;i--)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print((char)alphabet+" ");
+}
+alphabet--;
+System.out.println();
+}
+}
+}
+/* OUTPUT:
+E E E E E 
+D D D D 
+C C C 
+B B 
+A 
+*/
