f8f46070477ac9a3066f52283529d51e94e72b8a
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-22 15:35:17 +0530

    Pattern

---
+++ Dai3.java
@@ -0,0 +1,42 @@
+/*
+    *
+   * *
+  * * *
+ * * * *
+* * * * *  
+ * * * *
+  * * *
+   * *
+    *     
+ */
+package test;
+public class Dai3 
+{
+public static void main(String[] args) 
+{
+for(int i=1;i<=5;i++)
+{
+	for(int k=5;k>=i;k--)
+	{
+		System.out.print(" ");
+	}
+	for(int j=1;j<=i;j++)
+	{
+		System.out.print("*"+" ");
+	}
+	System.out.println();
+}
+for(int a=1;a<=4;a++)
+{
+	for(int c=a+1;c>=1;c--)
+	{
+		System.out.print(" ");
+	}
+	for(int b=4;b>=a;b--)
+	{
+		System.out.print("*"+" ");
+	}
+	System.out.println();
+}
+}
+}
