d54d0abd0963937da12a75df037db75f362e38ca
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-22 11:06:39 +0530

    Pattern

---
+++ N8.java
@@ -0,0 +1,39 @@
+/*
+1 2 3 4 5 6 7
+1 2 3 4 5 6
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2 
+1 
+1 2
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+1 2 3 4 5 6
+1 2 3 4 5 6 7
+ */
+package test;
+public class N8
+{
+public static void main(String[] args) 
+{
+for(int i=7;i>=1;i--)
+{
+	for(int j=1;j<=i;j++)
+	{
+		System.out.print(j+" ");
+	}
+	System.out.println();
+}
+for(int k=1;k<=6;k++)
+{
+	for(int n=1;n<=k+1;n++)
+	{
+		System.out.print(n+" ");
+	}
+	System.out.println();
+}
+}
+
+}
