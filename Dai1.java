225628ea72988a4814773a617848a13fe5bae4c1
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-22 14:16:42 +0530

    Pattern

---
+++ Dai1.java
@@ -0,0 +1,40 @@
+/*
+
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * * *
+* * * * *
+* * * *
+* * *
+* * 
+* 
+
+ */
+package test;
+public class Dai1
+{
+public static void main(String[] args) 
+{
+for(int i=1;i<=6;i++)
+{
+	for(int j=1;j<=i;j++)
+	{
+		System.out.print("*"+" ");
+	}
+	System.out.println();
+}
+for(int k=5;k>=1;k--)
+{
+	for(int s=k;s>=1;s--)
+	{
+		System.out.print("*"+" ");
+	}
+	System.out.println();
+}
+
+	}
+
+}
