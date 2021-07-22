be7013e1277608eca4cce9f628ffdc5325789495
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-22 09:05:37 +0530

    Pattern

---
+++ Pa7.java
@@ -0,0 +1,25 @@
+/*
+1
+2 3
+4 5 6
+7 8 9 10
+ */
+package test;
+public class Pa7
+{
+public static void main(String[] args) 
+{
+	int n=1;
+	for(int i=1;i<=4;i++)
+	{
+		
+		for(int j=i;j>=1;j--)
+		{
+			System.out.print(n+" ");
+			n++;
+		}
+		
+		System.out.println();
+	}
+}
+}
