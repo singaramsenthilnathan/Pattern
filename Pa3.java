d184b726c3f28cb32672f20ab516568f8f628841
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-22 09:03:47 +0530

    Pattern

---
+++ Pa3.java
@@ -0,0 +1,24 @@
+/*
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+ */
+package test;
+public class Pa3
+{
+public static void main(String[] args) 
+{
+for(int i=1;i<=5;i++)
+{
+	for(int j=i;j>=1;j--)
+	{
+		System.out.print("*"+" ");
+	}
+	System.out.println();
+}
+}
+}
