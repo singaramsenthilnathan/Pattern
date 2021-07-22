858eaf6723f14df4a9995dce0764ad35fcc5b150
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-22 09:03:19 +0530

    Pattern

---
+++ Pa2.java
@@ -0,0 +1,22 @@
+/*
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+ */
+package test;
+public class Pa2
+{
+public static void main(String[] args) 
+{
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=i;j>=1;j--)
+		{
+			System.out.print(i+" ");
+		}
+		System.out.println();
+	}
+}
+}
