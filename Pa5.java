4180f5617f00f3370f7161007b5df91c7d741384
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-22 09:04:39 +0530

    Pattern

---
+++ Pa5.java
@@ -0,0 +1,23 @@
+/*
+5 5 5 5 5
+4 4 4 4
+3 3 3
+2 2
+1
+ */
+
+package test;
+public class Pa5 
+{
+public static void main(String[] args) 
+{
+for(int i=5;i>=1;i--)
+{
+	for(int j=i;j>=1;j--)
+	{
+		System.out.print(i+" ");
+	}
+	System.out.println();
+}
+}
+}
