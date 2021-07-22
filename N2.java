bbdb1b4b87869c748309b60d4d2c974fc14da22b
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-22 10:34:09 +0530

    Pattern

---
+++ N2.java
@@ -0,0 +1,26 @@
+/*
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+6 6 6 6 6 6
+7 7 7 7 7 7 7
+ */
+package test;
+public class N2
+{
+public static void main(String[] args) 
+{
+	for(int i=1;i<=7;i++)
+	{
+		for(int j=i;j>=1;j--)
+		{
+			System.out.print(i+" ");
+		}
+		System.out.println();
+	}
+
+	}
+
+}
