08e2f9d67dca3a3d885ff6a978391685683258c2
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-22 11:37:02 +0530

    Pattern

---
+++ N11.java
@@ -0,0 +1,33 @@
+/*
+1
+10
+101
+1010
+10101
+101010 
+1010101
+ */
+package test;
+public class N11
+{
+public static void main(String[] args) 
+{
+for(int i=1;i<=7;i++)
+{
+	for(int j=1;j<=i;j++)
+	{
+		if(j%2==0)
+		{
+			System.out.print(0+" ");
+		}
+		else
+		{
+			System.out.print(1+" ");
+		}
+		
+	}
+	System.out.println();
+}
+}
+
+}
