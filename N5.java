a306c5f370b62038268c6c25a3420b15a9511414
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-22 10:44:38 +0530

    Pattern

---
+++ N5.java
@@ -0,0 +1,26 @@
+/*
+7 6 5 4 3 2 1
+6 5 4 3 2 1
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1
+ */
+package test;
+public class N5
+{
+public static void main(String[] args) 
+{
+for(int i=7;i>=1;i--)
+{
+	for(int j=i;j>=1;j--)
+	{
+		System.out.print(j+" ");
+	}
+	System.out.println();
+}
+
+	}
+
+}
