3bc599b9e5e507be746a1f11534729bec7ea8ae3
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-22 10:37:38 +0530

    Pattern

---
+++ N3.java
@@ -0,0 +1,26 @@
+/*
+7 6 5 4 3 2 1
+7 6 5 4 3 2 
+7 6 5 4 3 
+7 6 5 4
+7 6 5
+7 6
+7
+ */
+package test;
+public class N3
+{
+public static void main(String[] args) 
+{
+	for(int i=1;i<=7;i++)
+	{
+		for(int j=7;j>=i;j--)
+		{
+			System.out.print(j+" ");
+		}
+		System.out.println();
+	}
+
+	}
+
+}
