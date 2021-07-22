3b675f16a992fd02cdd28f2e158241b5c7c56a75
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-22 11:57:29 +0530

    Pattern

---
+++ N13.java
@@ -0,0 +1,29 @@
+/*
+1111111
+1111122
+1111333
+1114444
+1155555
+1666666
+7777777 
+ */
+package test;
+public class N13
+{
+public static void main(String[] args) 
+{
+for(int i=1;i<=7;i++)
+{
+	for(int k=6;k>=i;k--)
+	{
+		System.out.print("1"+" ");
+	}
+	for(int j=1;j<=i;j++)
+	{
+		System.out.print(i+" ");
+	}
+	System.out.println();
+}
+}
+
+}
