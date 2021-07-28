6b35a1ce421f61d827c32e100b5ec40b16a88b01
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-07-28 13:37:42 +0530

    Patterns

---
+++ P23.java
@@ -0,0 +1,26 @@
+/*
+      1
+     2 2
+   3 3 3
+  4 4 4 4
+ 5 5 5 5 5
+ */
+package test;
+public class P23
+{
+public static void main(String[] args) 
+{
+for(int i=1;i<=5;i++)
+{
+	for(int k=5;k>=i;k--)
+	{
+		System.out.print(" ");
+	}
+	for(int j=i;j>=1;j--)
+	{
+		System.out.print(i+" ");
+	}
+	System.out.println();
+}
+}
+}
